5cc802ac59a9e535b5396aa34f1b90b6a574bc24
Lasse <lasse.andresen9@gmail.com>
2023-04-08 22:50:37 +0200

    Expanded upon boardgameDetailComponent

--- src/app/frontend/components/boardgameDetail/boardgameDetailComponent.ts
+++ src/app/frontend/components/boardgameDetail/boardgameDetailComponent.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { BggThingDto } from "boardgamegeekclient/dist/esm/dto";
 import { BestPlayerCountPipe } from "./bestPlayerCountPipe";
+import { GamePublisherPipe } from "./gamePublisherPipe";
+import { DecimalPipe } from "@angular/common";
 
 @Component({
   templateUrl: './boardgameDetailComponent.html',
   selector: 'boardgame-details',
   standalone: true,
-  imports: [BestPlayerCountPipe]
+  imports: [BestPlayerCountPipe, GamePublisherPipe, DecimalPipe]
 })
 export class BoardgameDetailComponent implements OnDestroy, OnInit {
 
